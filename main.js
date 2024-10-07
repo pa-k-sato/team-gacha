a12a2b632940ebcb449218a89db4dc37c3eba8a4
kenji sato <63083111+pa-k-sato@users.noreply.github.com>
2024-10-07 15:55:28 +0900

    人数分の乱数を生成するロジックを実装
    Co-authored-by: pa-s-wada <pa-s-wada@users.noreply.github.com>
    Co-authored-by: pa-h-tokita <pa-h-tokita@users.noreply.github.com>
    Co-authored-by: pa-tetsuharu-watanabe <pa-tetsuharu-watanabe@users.noreply.github.com>
    Co-authored-by: pa-h-fukui <pa-h-fukui@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // 乱数を生成する関数をつくって
+function generateRandomNumber() {
+    const randomNumber = Math.floor(Math.random() * 6 + 1);
+    console.log(randomNumber);
+    return randomNumber;
+}
+
+generateRandomNumber()
