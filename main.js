054bf411ce1468916e574a98c6ab004248bb90bd
kenji sato <63083111+pa-k-sato@users.noreply.github.com>
2024-10-21 15:44:54 +0900

    人の割り当てができた
    Co-authored-by: pa-s-wada <pa-s-wada@users.noreply.github.com>
    Co-authored-by: pa-h-tokita <pa-h-tokita@users.noreply.github.com>
    Co-authored-by: pa-tetsuharu-watanabe <pa-tetsuharu-watanabe@users.noreply.github.com>
    Co-authored-by: pa-h-fukui <pa-h-fukui@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+// 乱数　＞　人に変換
+const persons = ['tokita', 'fukui', 'sato', 'watanabe', 'wada', 'tran'];
+
 // 乱数を生成する関数をつくって
-function generateRandomNumber() {
-    const randomNumber = Math.floor(Math.random() * 6 + 1);
-    console.log(randomNumber);
+function generateRandomNumber(number) {
+    const randomNumber = Math.floor(Math.random() * number + 1);
     return randomNumber;
 }
 
-generateRandomNumber()
+function displaySpeaker(number) {
+    const speaker = persons[number - 1];
+    console.log(number, speaker);
+}
+
+const number = generateRandomNumber(persons.length);
+displaySpeaker(number);
