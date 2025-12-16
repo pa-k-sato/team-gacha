1fbd7fca68f88b5b9fd65c9dbde6350ca8abec92
kenji sato <63083111+pa-k-sato@users.noreply.github.com>
2025-12-16 10:52:03 +0900

    update member

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // 乱数　＞　人に変換
-const persons = ['hayato.fukui', 'kenji.sato', 'tetsuharu.watanabe', 'keisuke.sato', 'eiichi.chida'];
+const persons = ['hayato.fukui', 'kenji.sato', 'keisuke.sato', 'eiichi.chida', 'yugo.okada'];
 
 function displaySpeaker(number) {
     const speaker = persons[number];
